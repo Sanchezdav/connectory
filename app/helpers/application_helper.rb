fb95a16d531f4b49d820b097a38686442f0a79f9
David <sanchez.dav90@gmail.com>
2021-04-03 22:31:06 -0600

    Create alerts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def alert_class_for(flash_type)
+    {
+      success: 'bg-green-400',
+      error: 'bg-red-400',
+      alert: 'bg-yellow-700',
+      notice: 'bg-blue-400'
+    }.stringify_keys[flash_type.to_s] || flash_type.to_s
+  end
 end
