1750a6ab0dc1ef7c13f0d1d9f82abdc65e92e78c
David <sanchez.dav90@gmail.com>
2021-04-03 23:00:19 -0600

    Create Posts scaffold

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_person_name
+  has_many :posts, dependent: :destroy
 
   validates :name, presence: true
 end
